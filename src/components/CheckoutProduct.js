786dd8caabb79c5c84c6206d213d30572055c018
guisantosalves <workguisantos@gmail.com>
2022-03-12 10:38:11 -0400

    making_checkout_2

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -8,7 +8,8 @@ function CheckoutProduct() {
             <img src="https://i.pinimg.com/564x/20/33/f0/2033f076f90d992b5e675fbede18bd40.jpg" className="checkoutProduct__image" />
 
             <div className="checkoutProduct__info">
-                <p className="checkoutProduct__title "></p>
+                <p className="checkoutProduct__title ">f type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem</p>
                 <p className="checkoutProduct__price">
                     <small>$</small>
                     <strong>20</strong>
